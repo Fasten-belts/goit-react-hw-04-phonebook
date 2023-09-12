5c94b3349a7e0ac4f4faeba75e1f50181ac760d3
Fasten-belts <124335932+Fasten-belts@users.noreply.github.com>
2023-09-12 14:45:45 +0200

    correct

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,21 +6,17 @@ import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
 
 function App() {
-  const [contacts, setContacts] = useState([
+  const init = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ];
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? init
+  );
 
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      setContacts(JSON.parse(savedContacts));
-    }
-  }, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
